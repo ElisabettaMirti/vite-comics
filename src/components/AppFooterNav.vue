a0dfed72945eb343e8212678816c401f19df15bb
ElisabettaMirti <belmirti@gmail.com>
2024-06-10 18:29:45 +0200

    inseriata navbar nel footer con contenuti dinamici e stile

--- src/components/AppFooterNav.vue
+++ src/components/AppFooterNav.vue
@@ -9,46 +9,46 @@ export default {
                 },
                 {
                     id: 2,
-                    name: 'comics',
+                    name: 'Comics',
                 },
                 {
                     id: 3,
-                    name: 'movies',
+                    name: 'Movies',
                 },
                 {
                     id: 4,
-                    name: 'tv',
+                    name: 'TV',
                 },
                 {
                     id: 5,
-                    name: 'games',
+                    name: 'Games',
                 },
                 {
                     id: 6,
-                    name: 'collectibles',
+                    name: 'Collectibles',
                 },
                 {
                     id: 7,
-                    name: 'videos',
+                    name: 'Videos',
                 },
                 {
                     id: 8,
-                    name: 'fans',
+                    name: 'Fans',
                 },
                 {
                     id: 9,
-                    name: 'news',
+                    name: 'News',
                 },
                 
             ],
             ShopLinks:[
                 {
                     id: 1,
-                    name: 'shop DC',
+                    name: 'Shop DC',
                 },
                 {
                     id: 2,
-                    name: 'shop DC collectibles',
+                    name: 'Shop DC Collectibles',
                 },
             ],
             DcLinks:[
@@ -125,9 +125,81 @@ export default {
 </script>
 
 <template>
+    <div class="footer-container">
+        <section class="navbar">
+            <nav>
+                <section class="nav-one">
+                    <h3>dc comics</h3>
+                    <ul>
+                        <li v-for="link in DcComicsLinks" :key="link.id">
+                            {{ link.name }}
+                        </li>
+                    </ul>
 
+                    <h3>shop</h3>
+                    <ul>
+                        <li v-for="link in ShopLinks" :key="link.id">
+                            {{ link.name }}
+                        </li>
+                    </ul>
+                </section>
+                <section class="nav-two">
+                    <h3>dc</h3>
+                    <ul>
+                        <li v-for="link in DcLinks" :key="link.id">
+                            {{ link.name }}
+                        </li>
+                    </ul>
+                </section>
+                <section class="nav-three">
+                    <h3>sites</h3>
+                    <ul>
+                        <li v-for="link in SitesLinks" :key="link.id">
+                            {{ link.name }}
+                        </li>
+                    </ul>
+                </section>
+            </nav>
+        </section>
+        <section class="dc-logo"></section>
+    </div>
+    
 </template>
 
 <style lang="scss" scoped>
+    @use '../styles/partials/variables.scss' as *;
+    @use '../styles/partials/mixins' as *;
+
+    div.footer-container{
+        padding: 0 20rem;
+        max-height: 400px;
+        display: flex;
+        justify-content: space-between;
+        background-image: url(../assets/img/footer-bg.jpg);
+    }
+
+    section.navbar{
+            padding: 2rem 1rem;
+
+            nav{
+                display: flex;
+
+                h3{
+                    color: white;
+                    text-transform: uppercase;
+                    margin: 1rem 0;
+                }
+            }
+        }
+
+    ul>li{
+        color: #707070;
+    }
+
+    .nav-one,
+    .nav-two,
+    .nav.three{
+        margin-right: 2rem;
+    }
 
 </style>
