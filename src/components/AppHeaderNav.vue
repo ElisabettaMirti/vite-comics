d7fe318f0f5d545cba2ef53dc07aad50bb6c7b57
ElisabettaMirti <belmirti@gmail.com>
2024-06-10 17:13:38 +0200

    inseriti data in AppHeaderNav

--- src/components/AppHeaderNav.vue
+++ src/components/AppHeaderNav.vue
@@ -1,5 +1,62 @@
 <script>
-
+export default {
+    data(){
+        return {
+            NavLinks: [
+                {
+                    id: 1,
+                    title: 'characters',
+                    url: '#'
+                },
+                {
+                    id: 2,
+                    title: 'comics',
+                    url: '#'
+                },
+                {
+                    id: 3,
+                    title: 'movies',
+                    url: '#'
+                },
+                {
+                    id: 4,
+                    title: 'tv',
+                    url: '#'
+                },
+                {
+                    id: 5,
+                    title: 'games',
+                    url: '#'
+                },
+                {
+                    id: 6,
+                    title: 'collectibles',
+                    url: '#'
+                },
+                {
+                    id: 7,
+                    title: 'videos',
+                    url: '#'
+                },
+                {
+                    id: 8,
+                    title: 'fans',
+                    url: '#'
+                },
+                {
+                    id: 9,
+                    title: 'news',
+                    url: '#'
+                },
+                {
+                    id: 10,
+                    title: 'shop',
+                    url: '#'
+                }
+            ]
+        }
+    }
+}
 </script>
 
 <template>
