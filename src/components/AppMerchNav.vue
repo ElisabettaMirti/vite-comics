ac175777f5a47cacc88ece30a121d4c0001462f6
ElisabettaMirti <belmirti@gmail.com>
2024-06-11 12:50:06 +0200

    inseriti data e method getImgPath nell'AppMerch

--- src/components/AppMerchNav.vue
+++ src/components/AppMerchNav.vue
@@ -2,7 +2,37 @@
 export default {
     data(){
         return {
+            MerchLinks: [
+                {
+                id: 1,
+                name: "digital comics",
+                logo: "buy-comics-digital-comics.png"
+                },
+                {
+                id: 2,
+                name: "dc merchandise",
+                logo: "buy-comics-merchandise.png"
+                },
+                {
+                id: 3,
+                name: "subscription",
+                logo: "buy-comics-subscriptions.png"
+                },
+                {
+                id: 4,
+                name: "comic shop locator",
+                logo: "buy-comics-shop-locator.png"
+                },
+                {
+                id: 5,
+                name: "dc power visa",
+                logo: "buy-dc-power-visa.svg"
+                },
+            ]
         }
+    },
+    methods: {
+
     }
 }
 </script>
