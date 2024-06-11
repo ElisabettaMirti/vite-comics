6f139dd42f24bc5aa93e3991d4dd2be6d6d0a789
ElisabettaMirti <belmirti@gmail.com>
2024-06-11 16:23:50 +0200

    inserito componente AppMerchNav con stile

--- src/components/AppMerchNav.vue
+++ src/components/AppMerchNav.vue
@@ -41,10 +41,47 @@ export default {
 
 <template>
 
+<section class="merch-nav">
+    <ul>
+        <li v-for="item in MerchLinks" :key="item.id">
+            <img :src="getImagePath(`../assets/img/${item.logo}`)" :alt="item.name">
+            <span>{{ item.name.toUpperCase() }}</span>
+        </li>
+    </ul>
+</section>
+
 </template>
 
 <style lang="scss" scoped>
 @use '../styles/general.scss';
 @use '../styles/partials/variables.scss' as *;
 @use '../styles/partials/mixins' as *;
+
+section.merch-nav{
+    background-color: $main-blue;
+    padding: 0 20rem;
+
+    ul{
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+        padding: 4rem 2rem;
+
+        li{
+            display: flex;
+            align-items: center;
+        }
+
+        li>span{
+            color: white;
+            margin-right: 2rem;
+        }
+
+        li>img{
+            height: 4rem;
+            max-width: 4rem;
+            margin-right: 1rem;
+        }
+    }
+}
 </style>
